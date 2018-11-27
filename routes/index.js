16d961b03ea46067062c12789695be97c97eef4a
nahilahmed <nahil757@gmail.com>
2018-11-28 01:30:44 +0530

    Final Commit

--- routes/index.js
+++ routes/index.js
@@ -67,15 +67,33 @@ router.post('/api/register',function(req,res,next){
   
 })
 router.post('/api/upload',upload.single('student'),function(req,res,next){
-  const filepath = __basedir+'/'+req.file.path
-  var options ={args:[filepath]}
-   ps.PythonShell.run(__basedir+'/resources/Student_Performance_Classifier/classifier.py',options,function(err,data){
-   if(!err)
-    res.send("Success"+data)
-    else
-    res.send("Error"+err)
- })
+  const filepath = __basedir+'/'+req.file.path;
+  const trainpath = __basedir+'/resources/Student_Performance_Classifier/xAPI-Edu-Data.csv'
+  var options ={args:[filepath,trainpath]}
+  var pyShell = new ps.PythonShell(__basedir+'/resources/Student_Performance_Classifier/classifier.py',options)
+  var output = []
+  pyShell.on('message',function(message){
+    if(message.length == 3){
+      output.push(message[2])
+    }else if(message.length == 4){
+      output.push(message[3])
+    }
+  })
+  pyShell.end(function (err) {
+    if (err){
+        throw err;}
+    else{
+      console.log(output.length)
+      res.render('test',{result:output})
+    } 
+    
+
+   
+});
 })
+// router.get('/result',function(req,res,next){
+//   res.render('result',{result:req.body.message})
+// })
 router.get('/dashboard',function(req,res,next){
   res.render('dashboard',{title:'Dashboard'})
 })
